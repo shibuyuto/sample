c7f3720f515515c501d3923e552a1a3a65db0805
Yuto Shibuya <sibuyuto@gmail.com>
2025-04-06 04:05:04 +0900

    Rename file to .nojekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	base: '/sample/',
+	site: 'https://github.com/shibuyuto/sample/',
 	integrations: [mdx(), sitemap()],
 });
